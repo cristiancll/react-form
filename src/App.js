9859615938aa6a2a0389ffc885c0e1a20cc74ce8
cristiancll <criscllorente@gmail.com>
2021-07-22 19:47:34 -0300

    -

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ class App extends Component{
   render() {
     return <Container component="article" maxWidth="sm">
               <Typography variant="h3" align="center" component="h1">Formulário de Cadastro</Typography>
-              <RegistrationForm/>  
+              <RegistrationForm submitForm={submitForm} validateTaxpayerId={validateTaxpayerId}/>  
            </Container>;
   }
 }
 
+function submitForm(form){
+    console.log(form);
+}
+function validateTaxpayerId(taxpayerId){
+    if(taxpayerId.length !== 11) return {error: true, helperText: "CPF deve ter 11 dígitos."};
+    return {error: false, helperText: ""};
+}
 export default App;
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, FormControlLabel, Switch, TextField} from "@material-ui/core";
 
-
-function RegistrationForm(){
-    return <form>
-        <TextField id="name" label="Nome" variant="outlined" margin="normal" fullWidth/>
-        <TextField id="surname" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
-        <TextField id="taxpayerId" label="CPF" variant="outlined" margin="normal" fullWidth/>
-        <FormControlLabel control={<Switch defaultChecked color="primary"/>} label="Promoções"/>
-        <FormControlLabel control={<Switch defaultChecked color="primary"/>} label="Novidades"/>
+function RegistrationForm({submitForm, validateTaxpayerId}){
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [taxpayerId, setTaxpayerId] = useState("");
+    const [promo, setPromo] = useState(true);
+    const [newsletter, setNewsletter] = useState(true);
+    
+    const [errors, setErrors] = useState({taxpayerId:{error: false, helperText: ""}});
+    
+    
+    
+    return <form onSubmit={event => {
+      event.preventDefault();
+      submitForm({name, surname, taxpayerId, promo, newsletter});
+    }}>
+        <TextField id="name" value={name} label="Nome" variant="outlined" margin="normal" fullWidth
+            onChange={event => setName(event.target.value)}/>
+        <TextField id="surname" value={surname} label="Sobrenome" variant="outlined" margin="normal" fullWidth
+            onChange={event => setSurname(event.target.value)}/>
+        <TextField id="taxpayerId" value={taxpayerId} label="CPF" variant="outlined" margin="normal" fullWidth
+            onChange={event => setTaxpayerId(event.target.value)}
+            onBlur={event => {
+                setErrors({taxpayerId: validateTaxpayerId(taxpayerId)})
+            }}
+            error={errors.taxpayerId.error}
+            helperText={errors.taxpayerId.helperText}/>
+        <FormControlLabel label="Promoções"
+            control={
+                <Switch color="primary"
+                    checked={promo}
+                    onChange={event => setPromo(event.target.checked)}/>
+            }/>
+        <FormControlLabel label="Novidades"
+            control={
+                <Switch color="primary"
+                    checked={newsletter}
+                    onChange={event => setNewsletter(event.target.checked)}/>
+            }/>
         <Button
             type="submit"
             variant="contained" 
@@ -17,5 +47,6 @@ function RegistrationForm(){
         </Button>
     </form>;
 }
+
 
 export default RegistrationForm;
